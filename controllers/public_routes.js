3a53222c0fe2a19468d1bd8295984e55dd1ba7db
Nicholas Zentai <nicholasz8687@gmail.com>
2023-04-20 14:17:01 -0400

    updated syntax private_routes and public_routes files

--- controllers/public_routes.js
+++ controllers/public_routes.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 
 function isLoggedIn(req, res, next) {
   if (req.session.user_id) return res.redirect('/dashboard');
-//  confirm path line 5
+//  confirm path line 5 - home.hbs
   next();
 }
 
 // Render the Homepage view
 router.get('/', isLoggedIn, (req, res) => {
   res.render('index');
-//   confirm path line 12
+//   confirm path line 12 - home.hbs
 });
 
 // Render the Login Page view
@@ -21,7 +21,7 @@ router.get('/login', isLoggedIn, (req, res) => {
 // Render the Register Page view
 router.get('/register', isLoggedIn, (req, res) => {
   res.render('auth/register');
-// confirm path line 23
+// confirm path line 23- register.hbs
 });
 
 module.exports = router;
